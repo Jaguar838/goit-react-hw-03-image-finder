5af68e2188abe2f18fa4c4fc1e5d552fae6da0a3
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-27 15:50:29 +0300

    Update index.js

--- src/UI/index.js
+++ src/UI/index.js
@@ -1,3 +1,5 @@
 export { Button } from './Button';
 export { Layout } from './Layout';
 export { SectionWrap } from './SectionWrap';
+export { Modal } from './Modal';
+export { Spinner } from './Spinner';
