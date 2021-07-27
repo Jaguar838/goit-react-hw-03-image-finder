1456538932cad721a11d662b2b3a99cd400dd4e4
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-27 16:24:56 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {Button} from './UI/Button';
 import PropTypes from 'prop-types';
 
 import css from './Searchbar.module.scss
 
 <header className="css.Searchbar">
   <form className="css.SearchForm">
-    <Button>
               <button type="submit" className="SearchForm-button">
                     <span className="SearchForm-button-label">Search</span>
                   </button>
