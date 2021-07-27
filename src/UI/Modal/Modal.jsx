3b8702b240c4bf484a469cdaa1bb66351f4b8a13
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-27 16:37:11 +0300

    Update Modal.jsx

--- src/UI/Modal/Modal.jsx
+++ src/UI/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from "prop-types";
 import s from './Modal.module.css';
