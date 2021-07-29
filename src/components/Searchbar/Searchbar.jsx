c24d54faf0198a684b9632c1d0ee2808cf1379d8
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-07-29 09:58:12 +0300

    add Searchbar component

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,34 +1,66 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
-// import css from './Searchbar.module.scss';
-
+import css from './Searchbar.module.scss';
+const INICIAL_STATE = {
+    request: '',
+};
 export class Searchbar extends Component {
-    state = {
-        value: '',
+    static propTypes = {
+        onSubmit: PropTypes.func.isRequired,
     };
-    // render() {
-    // return (<header className={css.Searchbar}>
-    //     <form className={css.SearchForm}>
-    //         <button type="submit" className={css.SearchForm - button}>
-    //             <span className={css.SearchForm - button - label}>
-    //                 Search
-    //             </span>
-    //         </button>
-    //         <input
-    //             className={css.SearchForm - input}
-    //             type="text"
-    //             autocomplete="off"
-    //             autofocus
-    //             placeholder="Search images and photos"
-    //         />
-    //     </form>
-    // </header>
-    // )
-    // }
+
+    state = INICIAL_STATE;
+
+    handleChangeSearch = ({ target }) => {
+        const { request, value } = target;
+        this.setState({ [request]: value.toLowerCase() });
+        console.log(value);
+    };
+
+    handleSubmit = evt => {
+        evt.preventDefault();
+        const { request } = this.state;
+        const { onSubmit } = this.props;
+        if (this.state.request.trim() === '') {
+            toast.error('Введите поисковый запрос');
+            return;
+        }
+        onSubmit({ request });
+        this.resetSearch();
+    };
+
+    resetSearch = () => {
+        this.setState(INICIAL_STATE);
+    };
+
+    render() {
+        const { request } = this.state;
+        return (
+            <header className={css.Searchbar}>
+                <form onSubmit={this.handleSubmit} className={css.SearchForm}>
+                    <button type="submit" className={css.SearchForm_button}>
+                        <span className={css.SearchForm_button_label}>
+                            Search
+                        </span>
+                    </button>
+                    <input
+                        className={css.SearchForm_input}
+                        type="text"
+                        autocomplete="off"
+                        autofocus
+                        placeholder="Search images and photos"
+                        onChange={this.handleChangeSearch}
+                        value={request}
+                    />
+                </form>
+            </header>
+        );
+    }
 }
 
 Searchbar.propTypes = {
     handleSubmit: PropTypes.func.isRequired,
-    handleChange: PropTypes.func.isRequired,
+    handleChangeSearch: PropTypes.func.isRequired,
     searchQuery: PropTypes.string.isRequired,
 };
