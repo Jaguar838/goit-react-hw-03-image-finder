a852507b46cbf0ad6d7b13d04cad635abb9cf46a
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-08-02 16:52:25 +0300

    Update apiService.js

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,6 +1,6 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api';
+// axios.defaults.baseURL = 'https://pixabay.com/api';
 // axios.defaults.params = {
 //     key: '21828776-3a3234db6b008ce4834511463',
 //     image_type: 'photo',
@@ -8,19 +8,25 @@ axios.defaults.baseURL = 'https://pixabay.com/api';
 //     per_page: 12,
 // };
 
-const fetchImg = ({ searchQuery = '', currentPage = 1 }) => {
-    return axios
-        .get('', {
-            params: {
-                key: '21828776-3a3234db6b008ce4834511463',
-                image_type: 'photo',
-                orientation: 'horizontal',
-                per_page: 12,
-                q: searchQuery,
-                page: currentPage,
-            },
-        })
-        .then(({ data }) => data.hits);
+// const fetchImg = async ({ query="dog", page}) => {
+//     const { data } = await axios
+//         .get("", {
+//             params: { q: query, page },
+//         });
+//     return data.hits;
+// };
+
+// export default fetchImg;
+
+import axios from 'axios';
+
+const apiKey = '4721358-13c427133c953f3e48d1d506d';
+const perPage = 12;
+const baseURL = `https://pixabay.com/api/?key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${perPage}&q=`;
+
+const fetchImg = ({ query = '', currentPage = 1 }) => {
+    const url = `${baseURL}${query}&page=${currentPage}`;
+    return axios.get(url).then(({ data }) => data.hits);
 };
 
 export default fetchImg;
