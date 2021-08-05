31e1f5f0baeb77fae62775301de8e13ef81b15f6
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-08-05 14:36:43 +0300

    Стили компонента Buton (Load more)

--- src/UI/Button/Button.style.js
+++ src/UI/Button/Button.style.js
@@ -1,16 +1,30 @@
 import styled from 'styled-components';
 
 export const Btn = styled.button`
-  cursor: pointer;
-  color: #7366ff;
-  font-weight: 500;
-  padding: 10px 15px;
-  border-radius: 5px;
-  background-color: rgba(115, 102, 255, 0.15);
-  transition: all 0.3s ease;
 
-  &:hover {
-    color: #fff;
-    background-color: #7366ff;
-  }
+.Button {
+  padding: 8px 16px;
+  border-radius: 2px;
+  background-color: #3f51b5;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  text-align: center;
+  display: inline-block;
+  color: #fff;
+  border: 0;
+  text-decoration: none;
+  cursor: pointer;
+  font-family: inherit;
+  font-size: 18px;
+  line-height: 24px;
+  font-style: normal;
+  font-weight: 500;
+  min-width: 180px;
+  box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+}
+
+.Button:hover,
+.Button:focus {
+  background-color: #303f9f;
+}
 `;
