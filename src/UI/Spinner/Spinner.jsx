5473959d2bd1f1c25d2a88c7fb9da5e94c231ec1
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-08-05 14:30:31 +0300

    type spinner Watch

--- src/UI/Spinner/Spinner.jsx
+++ src/UI/Spinner/Spinner.jsx
@@ -1,5 +1,5 @@
 import Loader from 'react-loader-spinner';
 
-const Spinner = () => <Loader color="#57ba41" size="23" type="Rings" />;
+const Spinner = () => <Loader color="#57ba41" size="23" type="Watch" />;
 
 export default Spinner;
